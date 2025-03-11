05ebd90451937cf9da7b96b8bbaa7c206adff2c8
NiloCK <colin@mrkennedy.ca>
2025-03-11 10:31:10 -0300

    init common-ui package

---
+++ packages/common-ui/src/index.ts
@@ -0,0 +1,15 @@
+// // Import and re-export components
+// import './styles/index.scss';
+
+// // Example component exports (you'll add your actual components here)
+// export { default as ExampleButton } from './components/ExampleButton.vue';
+// export { default as ExampleCard } from './components/ExampleCard.vue';
+
+// // Export any composables
+// export * from './composables';
+
+// // Export utility functions
+// export * from './utils';
+
+// // Export types
+// export * from './types';
