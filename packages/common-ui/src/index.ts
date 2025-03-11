016da864e1b7aea47e066975ea5128b434f54a16
NiloCK <colin@mrkennedy.ca>
2025-03-11 15:48:23 -0300

    export HeatMap

--- packages/common-ui/src/index.ts
+++ packages/common-ui/src/index.ts
@@ -1,9 +1,9 @@
 // // Import and re-export components
 // import './styles/index.scss';
 
-// // Example component exports (you'll add your actual components here)
-// export { default as ExampleButton } from './components/ExampleButton.vue';
-// export { default as ExampleCard } from './components/ExampleCard.vue';
+// component exports
+export { default as HeatMap } from './components/HeatMap.vue';
+export type { DayData, Color, ActivityRecord } from './components/HeatMap.types';
 
 // // Export any composables
 // export * from './composables';
