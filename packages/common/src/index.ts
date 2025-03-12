07e72bf621498600e77a39e7a3418704a0ecf943
NiloCK <colin@mrkennedy.ca>
2025-03-12 16:29:35 -0300

    bundle NonEmptyString on a Validators struct

--- packages/common/src/index.ts
+++ packages/common/src/index.ts
@@ -4,6 +4,7 @@ export { default as ENV } from './ENVIRONMENT_VARS.js';
 export * from './elo.js';
 export * from './namespacer.js';
 export * from './logshim.js';
+export * from './validators.js';
 
 // interfaces
 export * from './interfaces/index.js';
--- packages/common/src/validators.ts
+++ packages/common/src/validators.ts
@@ -1,18 +1,25 @@
-import { Validator, Status } from '@vue-skuilder/common';
+import { Validator } from './interfaces/Validator.js';
+import { Status } from './wire-format.js';
 
-export const NonEmptyString: Validator = {
-  instructions: '',
-  test: (input: string) => {
-    if (input.length !== 0) {
-      return {
-        status: Status.ok,
-        msg: '',
-      };
-    } else {
-      return {
-        status: Status.error,
-        msg: 'Input cannot be empty',
-      };
-    }
+interface ValidatorIndex {
+  [x: string]: Validator;
+}
+
+export const Validators: ValidatorIndex = {
+  NonEmptyString: {
+    instructions: '',
+    test: (input: string) => {
+      if (input.length !== 0) {
+        return {
+          status: Status.ok,
+          msg: '',
+        };
+      } else {
+        return {
+          status: Status.error,
+          msg: 'Input cannot be empty',
+        };
+      }
+    },
   },
 };
