e6560c3217af747a17f64e4566c3a6c5107d0b0c
NiloCK <colin@mrkennedy.ca>
2025-03-12 11:11:38 -0300

    consolidate validation interfaces

--- packages/vue/src/base-course/Interfaces/Validator.ts
+++ packages/vue/src/base-course/Interfaces/Validator.ts
@@ -1,4 +1,24 @@
-import { ValidatingFunction } from '../../base-course/Interfaces/ValidatingFunction';
+import { Status } from '@vue-skuilder/common';
+
+export type ValidatingFunction = (value: string) => ValidationResult;
+export type VuetifyRule = (value: string) => true | string;
+
+interface ValidationResult {
+  status: Status;
+  msg: string;
+}
+
+export function validationFunctionToVuetifyRule(f: ValidatingFunction): VuetifyRule {
+  return (value: string) => {
+    const result = f(value);
+
+    if (result.status === Status.ok) {
+      return true;
+    } else {
+      return result.msg;
+    }
+  };
+}
 
 export interface Validator {
   instructions?: string;
