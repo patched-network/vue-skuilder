dfc22b447c355f6c227530b42f77634fe2bf8925
NiloCK <colin@mrkennedy.ca>
2025-03-14 15:57:35 -0300

    remove dir barrel files

--- packages/common-ui/src/components/cardRendering/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './AudioAutoPlayer.vue';
-export * from './CodeBlockRenderer.vue';
-export * from './MarkdownRenderer.vue';
-export * from './MdTokenRenderer.vue';
--- packages/common-ui/src/components/studentInputs/index.ts
+++
@@ -1,7 +0,0 @@
-export * from './BaseUserInput';
-export * from './MultipleChoiceOption.vue';
-export * from './RadioMultipleChoice.vue';
-export * from './TrueFalse.vue';
-export * from './UserInputNumber.vue';
-export * from './UserInputString.vue';
-export * from './fillInInput.vue';
