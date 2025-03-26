17ee843645f6319b6fc1e589563fb59fbd82ad02
NiloCK <colin@mrkennedy.ca>
2025-03-26 14:03:39 -0300

    cypress updates to enable vuetify styling

--- packages/common-ui/cypress/support/component.js
+++ packages/common-ui/cypress/support/component.js
@@ -1,13 +1,21 @@
 // packages/common-ui/cypress/support/component.js
 import { mount } from 'cypress/vue';
 import { createVuetify } from 'vuetify';
+import * as components from 'vuetify/components';
+import * as directives from 'vuetify/directives';
+
+// Import Vuetify styles
 import 'vuetify/styles';
 import '@mdi/font/css/materialdesignicons.css';
+import './component-styles.css';
 
-// Create Vuetify instance
-const vuetify = createVuetify();
+// Create a more complete Vuetify instance
+const vuetify = createVuetify({
+  components,
+  directives,
+});
 
-// Add mount command directly without type checking
+// Add mount command
 Cypress.Commands.add('mount', (component, options = {}) => {
   // Initialize options if not provided
   if (!options.global) {
@@ -24,5 +32,20 @@ Cypress.Commands.add('mount', (component, options = {}) => {
     },
   });
 
-  return mount(component, options);
+  // Set up any global components needed
+
+  // Set up any specific Vuetify-related configurations
+  const el = document.createElement('div');
+  el.id = 'app';
+  document.body.appendChild(el);
+
+  // Add Vuetify CSS classes to the body
+  document.body.setAttribute('data-cy-vuetify', '');
+  document.body.classList.add('v-application');
+  document.body.classList.add('v-theme--light');
+
+  return mount(component, {
+    ...options,
+    attachTo: '#app',
+  });
 });
