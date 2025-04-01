ee43063d122bf8e27658c2dea632368e93878b07
NiloCK <colin@mrkennedy.ca>
2025-04-01 17:57:27 -0300

    expose factory, hide implementation

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -1,6 +1,8 @@
-// Re-export everything from the core and pouch implementations
 export * from './core';
-export * from './impl/pouch';
+
+// export * from './impl/pouch';
+
 export * from './study';
 
 export * from './util';
+export * from './factory';
