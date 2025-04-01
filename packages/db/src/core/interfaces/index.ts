8c09817e3a6dff1fe62d5b7d8088eec4bddce87a
NiloCK <colin@mrkennedy.ca>
2025-04-01 13:30:47 -0300

    remove redundant userDB interface spec

--- packages/db/src/core/interfaces/index.ts
+++ packages/db/src/core/interfaces/index.ts
@@ -3,5 +3,5 @@ export * from './classroomDB';
 export * from './contentSource';
 export * from './courseDB';
 export * from './dataLayerProvider';
-export * from './user';
+
 export * from './userDB';
