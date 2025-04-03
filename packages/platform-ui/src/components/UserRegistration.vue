36465a96b9456833afd6e53ad225eace1958fee9
NiloCK <colin@mrkennedy.ca>
2025-04-03 14:12:50 -0300

    use abstract user type

--- packages/platform-ui/src/components/UserRegistration.vue
+++ packages/platform-ui/src/components/UserRegistration.vue
@@ -62,7 +62,7 @@
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import { User } from '@vue-skuilder/db';
+import { UserDBInterface } from '@vue-skuilder/db';
 import { alertUser } from '@vue-skuilder/common-ui';
 import { Status, log } from '@vue-skuilder/common';
 import { getCurrentUser, useAuthStore } from '@/stores/useAuthStore';
@@ -83,7 +83,7 @@ export default defineComponent({
       badLoginAttempt: false,
       userSecret: '',
       secret: 'goons',
-      user: null as User | null,
+      user: null as UserDBInterface | null,
       roles: ['Student', 'Teacher', 'Author'] as string[],
       student: true,
       teacher: false,
