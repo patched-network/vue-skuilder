e4c3d4a1d060aff70df4009697174459d6f809ac
NiloCK <colin@mrkennedy.ca>
2025-04-16 15:51:14 -0300

    order imports to allow pkgs to init correctly

--- packages/platform-ui/src/main.ts
+++ packages/platform-ui/src/main.ts
@@ -1,3 +1,5 @@
+import ENV from './ENVIRONMENT_VARS';
+
 import 'roboto-fontface/css/roboto/roboto-fontface.css';
 import { createApp } from 'vue';
 import App from './App.vue';
@@ -6,10 +8,8 @@ import router from './router';
 import { createPinia } from 'pinia';
 import vuetify from './plugins/vuetify';
 // common-ui imports
-import { piniaPlugin } from '@vue-skuilder/common-ui';
 import '@vue-skuilder/common-ui/style';
-// `courses` imports
-import Courses from '@vue-skuilder/courses';
+// `courses` imports - keep style import for initial page rendering
 import '@vue-skuilder/courses/style';
 // `db` import and initialization
 import { initializeDataLayer } from '@vue-skuilder/db';
@@ -17,9 +17,17 @@ import { initializeDataLayer } from '@vue-skuilder/db';
 (async () => {
   await initializeDataLayer({
     type: 'pouch',
+    options: {
+      COUCHDB_SERVER_PROTOCOL: ENV.COUCHDB_SERVER_PROTOCOL,
+      COUCHDB_SERVER_URL: ENV.COUCHDB_SERVER_URL,
+    },
   });
+
   const pinia = createPinia();
   const app = createApp(App);
+
+  // Dynamically import Courses
+  const { default: Courses } = await import('@vue-skuilder/courses');
 
   // Register all view components globally
   const viewComponents = Courses.allViewsRaw();
@@ -30,6 +38,10 @@ import { initializeDataLayer } from '@vue-skuilder/db';
   app.use(router);
   app.use(vuetify);
   app.use(pinia);
+
+  // Dynamically import piniaPlugin
+  const { piniaPlugin } = await import('@vue-skuilder/common-ui');
   app.use(piniaPlugin, { pinia });
+
   app.mount('#app');
 })();
