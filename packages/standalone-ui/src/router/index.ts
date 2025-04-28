80bf6d5c5ddf5b28ae5492d7903aadba427c221e
NiloCK <colin@mrkennedy.ca>
2025-04-28 16:42:53 -0300

    add login/registration routs

--- packages/standalone-ui/src/router/index.ts
+++ packages/standalone-ui/src/router/index.ts
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router';
 import HomeView from '../views/HomeView.vue';
 import StudyView from '../views/StudyView.vue';
 import ProgressView from '../views/ProgressView.vue';
+import { UserLogin, UserRegistration } from '@vue-skuilder/common-ui';
 
 const routes: Array<RouteRecordRaw> = [
   {
@@ -18,6 +19,16 @@ const routes: Array<RouteRecordRaw> = [
     path: '/progress',
     name: 'progress',
     component: ProgressView,
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: UserLogin,
+  },
+  {
+    path: '/register',
+    name: 'Register',
+    component: UserRegistration,
   },
 ];
 
