b8b682183add18490e20e7fe92e75293d47fa9d1
NiloCK <colin@mrkennedy.ca>
2025-05-31 04:55:45 -0300

    allow unspecified return types

--- eslint.config.backend.mjs
+++ eslint.config.backend.mjs
@@ -1,19 +1,16 @@
 import tseslint from 'typescript-eslint';
 import baseConfig from './eslint.config.base.mjs';
 
-export default tseslint.config(
-  ...baseConfig,
-  {
-    languageOptions: {
-      parserOptions: {
-        project: true,
-      },
+export default tseslint.config(...baseConfig, {
+  languageOptions: {
+    parserOptions: {
+      project: true,
     },
-    rules: {
-      '@typescript-eslint/explicit-function-return-type': 'warn',
-      '@typescript-eslint/no-floating-promises': 'error',
-      '@typescript-eslint/await-thenable': 'error',
-      '@typescript-eslint/no-misused-promises': 'error',
-    },
-  }
-);
+  },
+  rules: {
+    '@typescript-eslint/explicit-function-return-type': 'off', // [ ] move back to warn eventually?
+    '@typescript-eslint/no-floating-promises': 'error',
+    '@typescript-eslint/await-thenable': 'error',
+    '@typescript-eslint/no-misused-promises': 'error',
+  },
+});
