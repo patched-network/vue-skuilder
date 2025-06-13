b78d8ce3e99113066b9794c1bc8676224fee6f85
NiloCK <colin@mrkennedy.ca>
2025-06-12 22:57:32 -0300

    add sync strategy interfaces

---
+++ packages/db/src/impl/common/index.ts
@@ -0,0 +1,10 @@
+// packages/db/src/impl/common/index.ts
+
+export { SyncStrategy, BaseSyncStrategy } from './SyncStrategy';
+export {
+  AccountCreationResult,
+  AuthenticationResult,
+  UserSession,
+  SyncConfig,
+  SyncStatus,
+} from './types';
