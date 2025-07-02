702f7c9f14249cb909f622f54b26e85e9abd4ac0
NiloCK <colin@mrkennedy.ca>
2025-07-02 15:15:13 -0300

    rm unused

--- packages/db/src/util/dataDirectory.test.ts
+++ packages/db/src/util/dataDirectory.test.ts
@@ -2,7 +2,6 @@
 import { describe, it, expect, beforeEach, afterEach } from 'vitest';
 import * as os from 'os';
 import * as path from 'path';
-import * as fs from 'fs';
 import { getAppDataDirectory, getDbPath, ensureAppDataDirectory } from './dataDirectory';
 
 describe('dataDirectory utilities', () => {
