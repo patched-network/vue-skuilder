ff661ba4a415181c25c73762b28e45713616ebf6
NiloCK <colin@mrkennedy.ca>
2025-07-07 15:17:36 -0300

    add title, logo props

--- packages/standalone-ui/src/components/CourseHeader.vue
+++ packages/standalone-ui/src/components/CourseHeader.vue
@@ -36,6 +36,12 @@ import { ref, computed } from 'vue';
 import { useDisplay } from 'vuetify';
 import { UserLoginAndRegistrationContainer } from '@vue-skuilder/common-ui';
 
+// Define props
+defineProps<{
+  title?: string;
+  logo?: string;
+}>();
+
 const { mobile } = useDisplay();
 const isMobile = computed(() => mobile.value);
 const drawer = ref(false);
