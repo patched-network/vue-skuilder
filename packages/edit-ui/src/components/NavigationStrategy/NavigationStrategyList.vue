1ef92ae3112a47d9487db405e80b1e0997eb50bb
NiloCK <colin@mrkennedy.ca>
2025-09-11 22:21:17 -0300

    add radioSelect for setting CourseConfig strategy

--- packages/edit-ui/src/components/NavigationStrategy/NavigationStrategyList.vue
+++ packages/edit-ui/src/components/NavigationStrategy/NavigationStrategyList.vue
@@ -1,45 +1,47 @@
 <template>
   <div class="navigation-strategy-list">
-    <v-list>
-      <v-list-item
-        v-for="strategy in strategies"
-        :key="strategy.id"
-        lines="three"
-      >
-        <template #prepend>
-          <v-icon> mdi-navigation </v-icon>
-        </template>
+    <v-radio-group :model-value="defaultStrategyId" @update:modelValue="$emit('update:defaultStrategy', $event)">
+      <v-list>
+        <v-list-item
+          v-for="strategy in strategies"
+          :key="strategy._id"
+          lines="three"
+        >
+          <template #prepend>
+            <v-radio :value="strategy._id"></v-radio>
+          </template>
 
-        <v-list-item-title class="text-h6">
-          {{ strategy.name }}
-        </v-list-item-title>
+          <v-list-item-title class="text-h6">
+            {{ strategy.name }}
+          </v-list-item-title>
 
-        <v-list-item-subtitle>{{ strategy.description }}</v-list-item-subtitle>
+          <v-list-item-subtitle>{{ strategy.description }}</v-list-item-subtitle>
 
-        <v-list-item-subtitle class="strategy-details mt-2">
-          <div><strong>Type:</strong> {{ strategy.implementingClass }}</div>
-          <div v-if="strategy.serializedData"><strong>Configuration:</strong> {{ getDisplayConfig(strategy) }}</div>
-        </v-list-item-subtitle>
+          <v-list-item-subtitle class="strategy-details mt-2">
+            <div><strong>Type:</strong> {{ strategy.implementingClass }}</div>
+            <div v-if="strategy.serializedData"><strong>Configuration:</strong> {{ getDisplayConfig(strategy) }}</div>
+          </v-list-item-subtitle>
 
-        <template #append>
-          <div class="d-flex">
-            <v-btn icon size="small" title="Edit Strategy (coming soon)" class="mr-1" disabled>
-              <v-icon>mdi-pencil</v-icon>
-            </v-btn>
+          <template #append>
+            <div class="d-flex">
+              <v-btn icon size="small" title="Edit Strategy (coming soon)" class="mr-1" disabled>
+                <v-icon>mdi-pencil</v-icon>
+              </v-btn>
 
-            <v-btn
-              icon
-              size="small"
-              title="Delete Strategy"
-              class="mr-1"
-              @click="$emit('delete', strategy)"
-            >
-              <v-icon>mdi-delete</v-icon>
-            </v-btn>
-          </div>
-        </template>
-      </v-list-item>
-    </v-list>
+              <v-btn
+                icon
+                size="small"
+                title="Delete Strategy"
+                class="mr-1"
+                @click="$emit('delete', strategy)"
+              >
+                <v-icon>mdi-delete</v-icon>
+              </v-btn>
+            </div>
+          </template>
+        </v-list-item>
+      </v-list>
+    </v-radio-group>
   </div>
 </template>
 
@@ -55,9 +57,13 @@ export default defineComponent({
       type: Array as PropType<ContentNavigationStrategyData[]>,
       required: true,
     },
+    defaultStrategyId: {
+      type: String as PropType<string | null>,
+      default: null,
+    },
   },
 
-  emits: ['edit', 'delete'],
+  emits: ['edit', 'delete', 'update:defaultStrategy'],
 
   methods: {
     getDisplayConfig(strategy: ContentNavigationStrategyData): string {
@@ -66,6 +72,9 @@ export default defineComponent({
       try {
         // Try to parse the serialized data to show a more user-friendly display
         const config = JSON.parse(strategy.serializedData);
+        if (Array.isArray(config)) {
+          return `${config.length} cards in sequence`;
+        }
         return Object.keys(config)
           .map((key) => `${key}: ${config[key]}`)
           .join(', ');
