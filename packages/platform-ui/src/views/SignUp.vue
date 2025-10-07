993d13f377cd670774ecbd42f7bdb9a2a4779b97
NiloCK <colin@mrkennedy.ca>
2025-10-07 11:52:08 -0300

    redirect to /u/[username]/new on pui signups

--- packages/platform-ui/src/views/SignUp.vue
+++ packages/platform-ui/src/views/SignUp.vue
@@ -1,6 +1,9 @@
 <template>
   <v-container class="d-flex align-center maxWidth">
-    <user-registration v-if="!isUserLoggedIn" />
+    <user-registration
+      v-if="!isUserLoggedIn"
+      @signup-success="handleSignupSuccess"
+    />
     <div v-else>Already logged in! Welcome back!</div>
   </v-container>
 </template>
@@ -20,6 +23,13 @@ export default defineComponent({
     isUserLoggedIn(): boolean {
       const authStore = useAuthStore();
       return authStore.isLoggedIn;
+    },
+  },
+
+  methods: {
+    handleSignupSuccess({ username }: { username: string }) {
+      console.log('[PLATFORM-UI] Signup success, redirecting to:', `/u/${username}/new`);
+      this.$router.push(`/u/${username}/new`);
     },
   },
 });
