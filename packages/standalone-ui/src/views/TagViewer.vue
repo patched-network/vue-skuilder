fcdab0de632e62494226545f64ad108ef4795a01
NiloCK <colin@mrkennedy.ca>
2025-11-26 13:51:55 -0400

    add tag-specifc views in standalone-ui

---
+++ packages/standalone-ui/src/views/TagViewer.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <h1>Tag: {{ tagId }}</h1>
+    <course-card-browser :course-id="courseId" :tag-id="tagId" :view-lookup-function="viewLookup" />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import { CourseCardBrowser } from '@vue-skuilder/common-ui';
+import { allCourseWare } from '@vue-skuilder/courseware';
+
+export default defineComponent({
+  name: 'TagViewer', // Changed name from TagEditorView
+  components: {
+    CourseCardBrowser,
+  },
+  props: {
+    tagId: {
+      type: String,
+      required: true,
+    },
+    courseId: {
+      type: String,
+      required: true,
+    },
+  },
+  methods: {
+    viewLookup(x: unknown) {
+      return allCourseWare.getView(x);
+    },
+  },
+});
+</script>
